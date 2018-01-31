52f96b9044ffd33d330ac79ff442cd49f767d2ad
Pr0tag0n1st <665825@dpsk12.net>
2018-01-31 09:26:11 -0700

    Super Saiyan Truck

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,33 @@
+#include<allegro5/allegro.h>
+#include<allegro5/allegro_primitives.h>
+
+int main() {
+
+	ALLEGRO_DISPLAY *display = NULL;
+
+	al_init();
+	al_init_primitives_addon();
+
+	display = al_create_display(600, 600);
+	
+	al_draw_filled_rectangle(100, 100, 500, 200, al_map_rgb(500, 200, 100));
+	al_draw_filled_circle(150, 200, 50, al_map_rgb(50, 50, 50));
+	al_draw_filled_circle(450, 200, 50, al_map_rgb(50, 50, 50));
+	al_draw_filled_rounded_rectangle(200, 100, 400, 50, 300, 10, al_map_rgb(500, 200, 100));
+	al_draw_filled_triangle(400, 50, 500, 100, 400, 100, al_map_rgb(200, 200, 255));
+
+	al_flip_display();
+	al_rest(5);
+	
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+
+
+
+
+
+
+
+}
